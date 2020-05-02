19ec55f71d2119db3f7a4179f601f9837c8f4bb8
Brunya <zbruno.linux@gmail.com>
2020-05-02 21:19:11 +0200

    formatting

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -2,120 +2,118 @@
   (:require [rum.core :as rum :refer [defcs]]))
 
 (defn section-title [text inverse]
-  [:div.section-title
+  [:.section-title
    [:div
     [:h2 {:class inverse} text]
     [:hr {:class [inverse]}]]])
 
 (defn hero []
   [:section.hero
-   [:div.flex-wrapper
-    [:div.title-bg
-      [:div
+   [:.flex-wrapper
+    [:.title-bg
+     [:div
       [:img {:src "/images/hero-logo.svg"}]
       [:p "is a privacy movement."]
       [:h1 "Send any cryptocurrencies" [:br] "completely anonymously."]
-      [:div.btn-wrapper
-        [:div.join-btn [:a {:href "https://incognito.org/"} "Join us"]]]]]
-    [:div.hero-img
-      [:img {:src "/images/arts/hero.svg"}]]]])
+      [:.btn-wrapper
+       [:.join-btn [:a {:href "https://incognito.org/"} "Join us"]]]]]
+    [:.hero-img
+     [:img {:src "/images/arts/hero.svg"}]]]])
 
 (defn about []
   [:section.about
-   [:div.container
+   [:.container
     (section-title "WHAT IS INCOGNITO?" nil)
-    [:div.flex-wrapper
-      [:div.about-text "Incognito is the first network that delivers privacy at scale by implementing proof-of-stake and sharding. It is not another cryptocurrency, but a privacy solution for your existing cryptocurrencies. The network is operated by a large group of small validators."]
-      [:div.img-wrapper
-       [:img.shard-art {:src "/images/arts/shard.svg"}]]]]])
+    [:.flex-wrapper
+     [:.about-text "Incognito is the first network that delivers privacy at scale by implementing proof-of-stake and sharding. It is not another cryptocurrency, but a privacy solution for your existing cryptocurrencies. The network is operated by a large group of small validators."]
+     [:.img-wrapper
+      [:img.shard-art {:src "/images/arts/shard.svg"}]]]]])
 
 (defn sign [value name]
-  [:div.block
-   [:div.value value]
-   [:div.name name]])
+  [:.block
+   [:.value value]
+   [:.name name]])
 
 (defn data []
   [:section.data
-   [:div.container
-    [:div.flex-wrapper
-      (sign "$1.54M" "shielded")
-      (sign "1257" "validators")
-      (sign "40" "dev members")
-      (sign "1.4M" "lines of code")]]])
+   [:.container
+    [:.flex-wrapper
+     (sign "$1.54M" "shielded")
+     (sign "1257" "validators")
+     (sign "40" "dev members")
+     (sign "1.4M" "lines of code")]]])
 
 (defn why []
   [:section.why
-   [:div.container
+   [:.container
     (section-title "WHY CHOOSE INCOGNITO?" nil)
-    [:div.flex-wrapper
-     [:div.block.flex-wrapper
-      [:div.img-container
-        [:img {:src "/images/arts/block.svg"}]]
-      [:div.text-container
-        [:p "10x faster " [:span "(100-800 TPS) "] "than any other privacy solutions " [:span "(usually less than 10 Transaction/sec)"]]]]
-     [:div.block.flex-wrapper
-      [:div.img-container
-        [:img {:src "/images/arts/block.svg"}]]
-      [:div.text-container
-        [:p "One PRV " [:span "($0.4) could "] "cover " [:span "your transaction & trading fees "] "in the next decade"]]]
-     [:div.block.flex-wrapper
-      [:div.img-container
-        [:img {:src "/images/arts/block.svg"}]]
-      [:div.text-container
-        [:p "It’s completely open-source " [:span "and contributed by members of a "] "global community"]]]]]])
-
-(def gif (atom "buy"))
+    [:.flex-wrapper
+     [:.block.flex-wrapper
+      [:.img-container
+       [:img {:src "/images/arts/block.svg"}]]
+      [:.text-container
+       [:p "10x faster " [:span "(100-800 TPS) "] "than any other privacy solutions " [:span "(usually less than 10 Transaction/sec)"]]]]
+     [:.block.flex-wrapper
+      [:.img-container
+       [:img {:src "/images/arts/block.svg"}]]
+      [:.text-container
+       [:p "One PRV " [:span "($0.4) could "] "cover " [:span "your transaction & trading fees "] "in the next decade"]]]
+     [:.block.flex-wrapper
+      [:.img-container
+       [:img {:src "/images/arts/block.svg"}]]
+      [:.text-container
+       [:p "It’s completely open-source " [:span "and contributed by members of a "] "global community"]]]]]])
 
 (defn function [name title desc side]
-  [:div.function {:on-click #(reset! gif name) :class (when (= name @gif) "active")}
-   (when (= "right" side) [:img {:src (str "/images/phone/" name "-icon.svg")}])
-   [:div
-    [:h4 title]
-    [:p desc]]
-   (when (= "left" side) [:img {:src (str "/images/phone/" name "-icon.svg")}])])
+  (let [gif (atom "buy")]
+    [:.function {:on-click #(reset! gif name) :class (when (= name @gif) "active")}
+     (when (= "right" side) [:img {:src (str "/images/phone/" name "-icon.svg")}])
+     [:div
+      [:h4 title]
+      [:p desc]]
+     (when (= "left" side) [:img {:src (str "/images/phone/" name "-icon.svg")}])]))
 
 (defn phone []
   [:section.phone
-   [:div.container
+   [:.container
     (section-title "INCOGNITO WALLET" "inverse")
-    [:div.flex-wrapper.background-circles
-      [:div.left-functions.flex-wrapper
+    [:.flex-wrapper.background-circles
+     [:.left-functions.flex-wrapper
       (function "buy" "Buy PRV" "PRV is the fuel of Incognito’s blockchain." "left")
       (function "shield" "Shield Crypt" "Put your money safe anonimously." "left")
       (function "wallet" "Use the Wallet" "Send and receive shielded crypto." "left")]
 
-      [:div.iphone
+     [:.iphone
       [:img {:src "/images/phone/phone.svg"}]]
 
-      [:div.right-functions.flex-wrapper
+     [:.right-functions.flex-wrapper
       (function "trade" "Trade" "Access to 70+ cryptos in decentralized exchange." "right")
       (function "stake" "Stake Nodes" "Be a part of the network and earn PRV passively." "right")
-      (function "forum" "Join the Forum" "Help the movement with your ideas and work." "right")]
-      ]]])
+      (function "forum" "Join the Forum" "Help the movement with your ideas and work." "right")]]]])
 
 (defn node []
   [:section.node
-   [:div.container
+   [:.container
     (section-title "INCOGNITO NODE" nil)
-    [:div.flex-wrapper
-      [:div.node-text
+    [:.flex-wrapper
+     [:.node-text
       [:h3 "Give people privacy. Earn passive income"]
       [:p "You could buy a physical node, or start operating your virtual node on your server. Prerequirements are staking 1750 PRV (~$700) which you could get back anytime if you change your mind."]
       [:p "Incognito nodes have a low power usage and they are ECO friendly."]
       [:p "Rewards from operating a node is random, but you could expect a decent 45%-100% yearly earning in the beginning."]
-      [:div.btn-wrapper
-        [:div.node-btn "Learn More"]]]
-      [:div.video-wrapper
+      [:.btn-wrapper
+       [:.node-btn "Learn More"]]]
+     [:.video-wrapper
       [:iframe {:src "https://www.youtube.com/embed/bafTu0kGfq4" :frameBorder "0" :allowFullScreen "allowFullScreen"}]]]]])
 
 (defn questions []
   [:section.questions
-   [:div.container
+   [:.container
     (section-title "QUESTIONS?" nil)
-    [:div.flex-wrapper
-      [:div.img-wrapper
+    [:.flex-wrapper
+     [:.img-wrapper
       [:img {:src "/images/arts/nodes.svg"}]]
-      [:div.questions-wrapper
+     [:.questions-wrapper
       [:h3 "Who made Incognito at the first place?"]
       [:p "A diverse group of 40+ scientists, cryptographers, programmers, entrepreneurs and operators worked on Incognito for 2 years before its launch."]
       [:h3 "How secure is using Incognito?"]
@@ -125,39 +123,39 @@
 
 (defn partners []
   [:section.partners
-   [:div.container
+   [:.container
     (section-title "YOU ARE IN A GOOD COMPANY" nil)
-    [:div.flex-wrapper
-      [:img {:src "/images/partners/binance.png"}]
-      [:img {:src "/images/partners/autonomous.png"}]
-      [:img {:src "/images/partners/neo.png"}]
-      [:img {:src "/images/partners/ontology.png"}]
-      [:img {:src "/images/partners/zilliqa.png"}]
-      [:img {:src "/images/partners/harmony.png"}]]]])
+    [:.flex-wrapper
+     [:img {:src "/images/partners/binance.png"}]
+     [:img {:src "/images/partners/autonomous.png"}]
+     [:img {:src "/images/partners/neo.png"}]
+     [:img {:src "/images/partners/ontology.png"}]
+     [:img {:src "/images/partners/zilliqa.png"}]
+     [:img {:src "/images/partners/harmony.png"}]]]])
 
 (defn ecosystem []
   [:section.ecosystem
-   [:div.container
+   [:.container
     (section-title "OUR ECOSYSTEM" nil)
-    [:div.flex-wrapper
-      [:div.card
-       [:h3 "Community Forum"]
-       [:img {:src "/images/ecosystem/forum.png"}]]
-      [:div.card
-       [:h3 "pDEX Statistics"]
-       [:img {:src "/images/ecosystem/pdex.png"}]]
-      [:div.card
-       [:h3 "Incognito Explorer"]
-       [:img {:src "/images/ecosystem/explorer.png"}]]]]])
+    [:.flex-wrapper
+     [:.card
+      [:h3 "Community Forum"]
+      [:img {:src "/images/ecosystem/forum.png"}]]
+     [:.card
+      [:h3 "pDEX Statistics"]
+      [:img {:src "/images/ecosystem/pdex.png"}]]
+     [:.card
+      [:h3 "Incognito Explorer"]
+      [:img {:src "/images/ecosystem/explorer.png"}]]]]])
 
 (defn spotlight []
   [:section.spotlight
-   [:div.container
+   [:.container
     (section-title "PROJECT SPOTLIGHT" "inverse")
-    [:div.flex-wrapper
-      [:img {:src "/images/white-zgen-logo.svg"}]
-      [:div.spotlight-text "This website was made by members of the ZGEN DAO. We created it, because we believe we found a hidden gem of the cryptocurrency world and we decided to introduce Incognito to a wider audience. "]]
-    [:div.zgen-desc "ZGEN is a bureaucracy-free online guild of makers & entrepreneurs. Our community is driven by goals & needs of the generation Z."]]])
+    [:.flex-wrapper
+     [:img {:src "/images/white-zgen-logo.svg"}]
+     [:.spotlight-text "This website was made by members of the ZGEN DAO. We created it, because we believe we found a hidden gem of the cryptocurrency world and we decided to introduce Incognito to a wider audience. "]]
+    [:.zgen-desc "ZGEN is a bureaucracy-free online guild of makers & entrepreneurs. Our community is driven by goals & needs of the generation Z."]]])
 
 (defcs app []
        [:div
