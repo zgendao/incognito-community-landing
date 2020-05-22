1c575801b3182833181ad9c0c97c50f9d0e46d1e
Brunya <zbruno.linux@gmail.com>
2020-05-22 14:40:07 +0200

    ackee

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,9 +1,12 @@
 (ns app.core
   (:require [rum.core :as rum]
-            [app.views :as views]))
+            [app.views :as views]
+            [app.peekaboo :as peekaboo]))
 
 (defn ^:dev/after-load start []
   (rum/mount (views/app) (.getElementById js/document "app")))
 
 (defn ^:export main []
-  (start))
+  (do
+    (start)
+    (peekaboo/gift "c72e5bee-8ec7-4fc3-904b-0bb87060f9cd")))
