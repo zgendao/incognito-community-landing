2921821e299f5cf8ca4ed4a05effc1f959fa0564
R.A.Z <raz@zgen.hu>
2020-07-14 16:45:04 +0200

    svg fix

--- src/app/heroArt_.cljs
+++ src/app/heroArt_.cljs
@@ -1,6 +1,11 @@
 (ns app.heroArt_
   (:require [rum.core :as rum :refer [defcs defc reactive react cursor]]
-            ["react-samy-svg" :as Samy]))
+            ["react-svgmt" :refer [SvgLoader]]))
 
-(defc heroArt_ []
-  [:Samy {:path "/images/arts/hero.svg"}])
+(defc heroArt_ < rum/static
+  []
+  (js/React.createElement 
+    SvgLoader
+    #js {:path "/images/arts/hero.svg"} 
+    )
+  )
